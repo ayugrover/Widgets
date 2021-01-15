3a5a9ae0e2aa85dc52f4dd3d896b91e041632847
Ayushi <ayushigrover001@gmail.com>
2021-01-15 13:04:07 +0530

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Accordion from './Components/Accordion';
+import Search from './Components/Search';
+import Dropdown from './Components/Dropdown';
+import Translate from './Components/Translate';
+import Route from './Components/Route';
+import Header from './Components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items = [
+{
+   title: 'What is React',
+   content : 'React is a front end js framework'
+},
+{
+  title: 'Why use react?',
+  content : 'Its a fovourite lib among engineers'
+},
+{
+  title: 'How do you use react?',
+  content : 'By creating components'
+}
+];
+
+const options = [
+  {
+     label: 'Red',
+     value : 'Red'
+  },
+  {
+     label: 'Yellow',
+     value : 'Yellow'
+  },
+  {
+     label: 'Blue',
+     value : 'Blue'
+  }
+  ];
+
+const App =  () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return(
+    <div> 
+      <Header/>
+     <Route path ="/">
+       <Accordion items={items}/>
+     </Route>
+     <Route path ="/list">
+       <Search />
+     </Route>
+     <Route path ="/dropdown">
+     <Dropdown 
+            label = "Select a Colour"
+            options= {options}
+            selected = {selected}
+            onSelectedChange ={setSelected}
+            />
+     </Route>
+     <Route path ="/translate">
+       <Translate />
+     </Route>
     </div>
   );
-}
+};
 
 export default App;
